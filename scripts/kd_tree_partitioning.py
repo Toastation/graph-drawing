9c1bf402037c62e69788e72da279e20893621b7d
Toastation <toastations@gmail.com>
2018-05-19 01:09:45 +0200

    added coloring for partitioning

--- scripts/kd_tree_partitioning.py
+++ scripts/kd_tree_partitioning.py
@@ -1,14 +1,18 @@
 from tulip import tlp
 from collections import deque
 import math
+import random
 
 ITERATIONS = 4
 PARTITION_SIZE = 5
 
 def main(graph):
     layout = graph.getLayoutProperty("viewLayout")
+    colors = graph.getColorProperty("viewColor")
     partitions = deque([graph.nodes()])
+
     quit = False
+    count = 0
 
     while not quit:
         nb_partitions = len(partitions)
@@ -27,7 +31,16 @@ def main(graph):
             # terminate when we have reached the ideal partition size 
             if p_size // 2 <= PARTITION_SIZE: 
                 quit = True
+                
+        count += 1
     
     partitions = list(partitions)
-    print(partitions)
     
+    # DEBUG
+    print(partitions)
+    for p in partitions:
+        r = random.randrange(255)
+        g = random.randrange(255)
+        b = random.randrange(255)
+        for n in p:
+            colors[n] = tlp.Color(r, g, b)  
