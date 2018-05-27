219461f03bbe750713b0b150b16668f6e799710d
Toastation <toastations@gmail.com>
2018-05-28 00:15:43 +0200

    added computation of the distance_to_node series of set

--- scripts/pinning_weights.py
+++ scripts/pinning_weights.py
@@ -5,16 +5,17 @@ NEIGHBOR_INFLUENCE = 0.6    # [0, 1], a higher value will reduce the influence o
 PINNING_WEIGHT_INIT = 0.35  # determines the decay of the pinning weight
 K = 0.5                     # determines how much a change should spread
 
-## Computes the positioning score of each node, and place new nodes according to their number of positioned neighbors.
+## \brief Computes the positioning score of each node, and place new nodes according to their number of positioned neighbors.
 # These scores indicate the "confidence" in the node's position (comprised between [0, 1]).
 # A score of 1 is assigned to nodes already in the graph, 0.25 to new nodes with 2 or more positioned neighbors,
 # 0.1 to to new nodes with 1 positioned neighbor and 0 to new nodes with no neighbors.
 # New nodes with 2 more positioned neighbors are place at their weighted barycenter, new nodes with 1 positioned
 # neighbor are place between their neighbor and the center of the graph, and new nodes with no positioned neighbors
 # are placed in a circle around the center of the graph.
+# \param graph The graph on which to compute the scores
 def compute_positioning_score(graph):
+    positioned = graph.getLocalBooleanProperty("positioned")    
     is_new_node = graph.getBooleanProperty("isNewNode")
-    positioned = graph.getBooleanProperty("positioned")    
     positioning_scores = graph.getDoubleProperty("positioningScore")    
     layout = graph.getLayoutProperty("viewLayout")
     center_coord = tlp.computeBoundingBox(graph).center()
@@ -29,11 +30,13 @@ def compute_positioning_score(graph):
 
     for n in new_nodes_graph.bfs():
         positioned_neighbors = [neighbor for neighbor in graph.getInOutNodes(n) if positioned[neighbor]]
-        nb_positioned = len(unpositioned_neighbors)       
+        nb_positioned = len(unpositioned_neighbors)    
+
         if nb_positioned >= 2:
             positioning_scores[n] = 0.25
             layout[n] = tlp.Vec3f()
-            for neighbor in positioned_neighbors: layout[n] += layout[neighbor]
+            for neighbor in positioned_neighbors: 
+                layout[n] += layout[neighbor]
             layout[n] /=  nb_positioned
             positioned[n] = True
         elif nb_positioned == 1:
@@ -43,16 +46,39 @@ def compute_positioning_score(graph):
             positioning_scores[n] = 0            
             layout[n] = tlp.Vec3f(center_coord.x() + radius * math.cos(alpha), center_coord.x() + radius * math.sin(alpha))
             alpha += dalpha
-    
-    graph.delSubGraph(new_nodes_graph)        
-    
+    graph.delSubGraph(new_nodes_graph)
+    graph.delLocalProperty("positioned")    
 
+## \brief Computes a series of sets in the following manner:
+# D_0 = the set of new nodes and nodes adjacent to a removed node
+# D_i+1 = the set of nodes adjacent to a node in D_i and not yet in a set
+# \param graph The graph from which to computes the sets
+# \return A list of list of tlp.node
 def compute_distance_to_node(graph):
-    # TODO: if a BFS way, create a series of sets where D0 = [pinning_weights[n] < 1] U [nodes adjacent to a change]
-    # and Di+1 = [nodes not in a set and ajdacent to Di]
-    # should return the list of sets (?)
-    pass
+    in_set = graph.getLocalBooleanProperty("inSet") # has a node been added to a set
+    is_new_node = graph.getBooleanProperty("isNewNode")    
+    is_adjacent_to_deleted = graph.getBooleanProperty("isAdjacentToDeleted")
+    pinning_weights = graph.getDoubleProperty("pinningWeight")    
+    distance_to_node = [[n for n in graph.getNodes() if is_new_node[n] or is_adjacent_to_deleted[n]]] # D0    
+    current_set_index = 0
+    while True:
+        next_set = []
+        for n in distance_to_node[current_set_index]:
+            for neighbor in graph.getInOutNodes(n):
+                if not in_set[neighbor]:
+                    next_set.append(neighbor)
+        if len(next_set) == 0: break 
+        distance_to_node.append(next_set)
+        current_set_index++
+    return distance_to_node
 
+## \brief Computes and assign a pinning weight to every node of the graph.
+# Pinning weights indicate how much a node will be able to move during the layout algo.
+# Pinning weights are comprised in the interval [0, 1]. A node with a pinning weight of 1
+# will not move during the layout algo whereas a node with a pinning weight of 0 will be 
+# completely free to move.
+# \param graph The graph on which to compute the pinning weights
+# \param neighbor_influence [0, 1], a higher value will reduce the influence of the neighbors of a node on its displacement
 def compute_pinning_weights(graph, neighbor_influence):
     positioning_scores = graph.getDoubleProperty("positioningScore")
     pinning_weights = graph.getDoubleProperty("pinningWeight")
