af8ee6abcf4eca431e358bc4ab19055a1219ae1a
Toastation <toastations@gmail.com>
2018-05-30 19:41:05 +0200

    restructuring

---
+++ scripts/frishman-tal/basic_fr.py
@@ -0,0 +1,52 @@
+# basic implementation of the Fruchterman-Reingold algorithm, not efficient
+def run(graph, iterations):
+    boundingbox = tlp.computeBoundingBox(graph) 
+    width = boundingbox.width()
+    height = boundingbox.height()
+    area = width * height
+    min_coord = boundingbox[0]
+    max_coord = boundingbox[1]
+    
+    layout = graph.getLayoutProperty("viewLayout")
+    forces = graph.getLayoutProperty("forces")          # node:tlp.Vec3f dict representing the total forces applied on each node 
+    
+    nb_nodes = graph.numberOfNodes()
+    k = math.sqrt(area / nb_nodes)                      # ideal length between nodes given the above force model 
+    temp = min(width, height) / 10
+    dt = temp / (iterations + 1)
+
+    # init forces
+    for n in graph.nodes():
+        forces[n] = tlp.Vec3f()
+
+    for i in range(iterations):
+        # computing repulsive forces for every pair of nodes
+        for n in graph.getNodes():
+            for n2 in graph.getNodes():
+                if n != n2:
+                    delta_pos = layout[n] - layout[n2]
+                    dist = delta_pos.norm()
+                    if dist != 0: # TODO: push the nodes if dist == 0
+                        forces[n] += delta_pos * (repulsive_force(dist, k) / dist)
+
+        # computing attractive forces for every edge
+        for e in graph.getEdges():
+            source = graph.source(e)
+            target = graph.target(e)
+            delta_pos = layout[source] - layout[target]
+            dist = delta_pos.norm()
+            if dist != 0: # randomly generated graphs may have edges that connect the same node...
+                force = attractive_force(dist, k) / dist 
+                forces[source] -= delta_pos * force
+                forces[target] += delta_pos * force
+
+        # updating nodes position and keeping them in-bound
+        for n in graph.getNodes():
+            force = forces[n].norm()
+            if force != 0:
+                displacement = min(force, temp) / force
+                layout[n] += forces[n] * displacement
+                layout[n].setX(min(min_coord.x(), max(max_coord.x(), layout[n].x())))
+                layout[n].setY(min(min_coord.y(), max(max_coord.y(), layout[n].y())))                
+            forces[n].fill(0)
+        temp = cool(temp, dt)
