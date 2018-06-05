aad01ee50ec2ec99107f287b823be43fa8c044b5
Toastation <toastations@gmail.com>
2018-06-05 03:09:43 +0200

    layout

--- scripts/FMMM-incremental/fmmm-layout.py
+++ scripts/FMMM-incremental/fmmm-layout.py
@@ -1,9 +1,20 @@
 from tulip import tlp
+from anytree import NodeMixin
 import math
 
 DEFAULT_ITERATIONS = 1000
 REPL_CONST = 4.0
 DL = 0.055
+P = 4   # P-term multipole expansion
+
+class KDTree(NodeMixin):
+    _p = P
+
+    def __init__(self, parent, vertices):
+        super(NodeMixin, self).__init__()
+        self.parent = parent
+        self.vertices = vertices
+        self._compute()
 
 class FMMMLayout:
 
@@ -19,7 +30,23 @@ class FMMMLayout:
         dist_norm = dist.norm()
         return dist_norm * math.log(dist_norm / self._desired_edge_length) * dist
 
-    def run(self, graph, iterations=self._default_iterations):
-        pass
+    def _compute_node(graph, node):
+        pos = graph.getLayoutProperty("viewLayout")
+        if node.vertices:
+                print("Cannot compute kd tree node {}: no vertices".format(node))
+            return False
+        (center, farthest_point) = tlp.computeBoundingRadius(graph)
+        node.center = center
+        node.radius = self.center.dist(farthest_point)
 
-    
+        # compute potential of vertices
+        
+
+
+    def _create_kd_tree(graph):
+        root = KDTree(None, graph.nodes())
+
+
+        
+    def run(self, graph, iterations = self._default_iterations):
+        pass
